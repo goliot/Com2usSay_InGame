b83c184ea0e617212d1c5e9a516911f385efbe6d
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-06-09 16:39:53 +0900

    250609 / readonly를 json으로 만들려던 버그 수정

--- Assets/01. Scripts/Currency/2. Repository/CurrencyRepository.cs
+++ Assets/01. Scripts/Currency/2. Repository/CurrencyRepository.cs
@@ -6,25 +6,31 @@ public class CurrencyRepository
     private readonly string SAVE_KEY = nameof(CurrencyRepository);
 
     // 영속성 -> 게임을 꺼도 데이터 보존
+    // Save
     public void Save(List<CurrencyDTO> dataList)
     {
-        CurrencySaveData data = new CurrencySaveData();
-        data.DataList = dataList;
+        CurrencySaveDatas datas = new CurrencySaveDatas();
+        datas.DataList = dataList.ConvertAll(data => new CurrencySaveData
+        {
+            Type = data.Type,
+            Value = data.Value
+        });
 
-        string json = JsonUtility.ToJson(data);
+        string json = JsonUtility.ToJson(datas);
         PlayerPrefs.SetString(SAVE_KEY, json);
     }
 
+    // Load
     public List<CurrencyDTO> Load()
     {
-        if(!PlayerPrefs.HasKey(SAVE_KEY))
+        if (!PlayerPrefs.HasKey(SAVE_KEY))
         {
             return null;
         }
 
         string json = PlayerPrefs.GetString(SAVE_KEY);
-        CurrencySaveData data = JsonUtility.FromJson<CurrencySaveData>(json);
+        CurrencySaveDatas datas = JsonUtility.FromJson<CurrencySaveDatas>(json);
 
-        return data.DataList;
+        return datas.DataList.ConvertAll<CurrencyDTO>(data => new CurrencyDTO(data.Type, data.Value));
     }
 }
