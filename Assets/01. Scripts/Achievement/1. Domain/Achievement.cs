c974a3eab7603938b8cbc7bb60ccccc7567db618
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-06-10 09:55:47 +0900

    250610 / 업적시스템 도메인 계층

---
+++ Assets/01. Scripts/Achievement/1. Domain/Achievement.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+public class Achievement
+{
+    [Header("# 데이터")]
+    public readonly string Name;
+    public readonly string Description;
+    public readonly EAchievementCondition Condition;
+    public int GoalValue;
+    public ECurrencyType RewardCurrencyType;
+    public int RewardAmount;
+
+    [Header("# 상태")]
+    private int _currentValue;
+    public int CurrentValue => _currentValue;
+
+    private bool _isRewardClaimed;
+    public bool IsRewardClaimed => _isRewardClaimed;
+
+    public Achievement(string name, string description, EAchievementCondition condition, int goalValue, ECurrencyType rewardCurrencyType, int rewardAmount)
+    {
+        if(string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentException("업적 이름은 비어있을 수 없습니다.");
+        }
+        if (string.IsNullOrEmpty(description))
+        {
+            throw new ArgumentException("업적 설명은 비어있을 수 없습니다.");
+        }
+        if(goalValue <= 0)
+        {
+            throw new ArgumentException("업적 목표 값은 0보다 커야 합니다.");
+        }
+        if(rewardAmount <= 0)
+        {
+            throw new ArgumentException("업적 보상은 0보다 커야 합니다.");
+        }
+
+        Name = name;
+        Description = description;
+        Condition = condition;
+        GoalValue = goalValue;
+        RewardCurrencyType = rewardCurrencyType;
+        RewardAmount = rewardAmount;
+    }
+
+    public void Increase(int value)
+    {
+        if(value <= 0)
+        {
+            throw new ArgumentException("0 이하를 증가시킬 수 없습니다.");
+        }
+
+        _currentValue += value;
+    }
+}
--- Assets/01. Scripts/Currency/1. Domain/ECurrencyType.cs
+++ Assets/01. Scripts/Currency/1. Domain/ECurrencyType.cs
@@ -2,6 +2,6 @@ public enum ECurrencyType
 {
     Gold,
     Diamond,
-
+    Ruby,
     Count,
 }
