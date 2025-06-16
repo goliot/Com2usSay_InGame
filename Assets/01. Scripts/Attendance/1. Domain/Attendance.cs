a851e00aa6c91d7067f9b14811547398e749d542
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-06-16 09:54:41 +0900

    256016 / 출석 도메인

--- Assets/01. Scripts/Attendance/1. Domain/Attendance.cs
+++ Assets/01. Scripts/Attendance/1. Domain/Attendance.cs
@@ -1,16 +1,36 @@
-using UnityEngine;
+using System;
 
-public class Attendance : MonoBehaviour
+public class Attendance
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public DateTime StartDate; // 출석 이벤트를 언제부터 시작할 것인가 -> 패치 없이 UI를 노출하기 위함
+    public int DayCount; // 출석 횟수
+    public DateTime LastAttendanceDate; // 마지막 출석일
 
-    // Update is called once per frame
-    void Update()
+    public Attendance(DateTime startDate, int dayCount, DateTime lastAttendanceDate)
     {
-        
+        if(startDate == default)
+        {
+            throw new Exception("출석 시작일이 지정되지 않았습니다.");
+        }
+        if(dayCount <= 0)
+        {
+            throw new Exception("출석 횟수는 0 이하일 수 없습니다.");
+        }
+        if (lastAttendanceDate == default)
+        {
+            throw new Exception("마지막 출석일이 지정되지 않았습니다.");
+        }
+        if (lastAttendanceDate < startDate)
+        {
+            throw new Exception("마지막 출석일은 시작일 이전일 수 없습니다.");
+        }
+        if (lastAttendanceDate > DateTime.Today)
+        {
+            throw new Exception("마지막 출석일은 오늘 이후일 수 없습니다.");
+        }
+
+        StartDate = startDate;
+        DayCount = dayCount;
+        LastAttendanceDate = lastAttendanceDate;
     }
 }
