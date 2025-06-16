64f2d121282604b66b79461c5958aa843a7313af
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-06-16 16:40:46 +0900

    250616 /  스크립트 빠진거 찾음

---
+++ Assets/01. Scripts/Stage/4. UI/UI_Stage_Test.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class UI_Stage_Test : MonoBehaviour
+{
+    public TextMeshProUGUI StageTypeText;
+    public TextMeshProUGUI CurrentStageText;
+    public TextMeshProUGUI TimerText;
+
+    private void Start()
+    {
+        Refresh();
+
+        StageManager.Instance.OnLevelChangeEvent += Refresh;
+    }
+
+    private void Refresh()
+    {
+        StageDTO dto = StageManager.Instance.StageDto;
+
+        StageTypeText.text = nameof(dto.StageType);
+        CurrentStageText.text = dto.CurrentLevel.ToString();
+        TimerText.text = StageManager.Instance.Timer.ToString();
+    }
+}
